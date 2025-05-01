84402386550417ab1efb8c4b4fbdcf17d2324277
hmeberbeche <hichemamir.meberbeche@cegedim-sante.com>
2025-05-01 23:51:41 +0200

    Update build script in package.json to use Doppler for environment variable management; add favicon icon metadata in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,9 @@ const poppins = Poppins({
 export const metadata: Metadata = {
   description:
     'Ludora est l’appli qui te connecte instantanément à des partenaires pour jouer au foot, basket, padel et plus. Crée ou rejoins un match, réserve un terrain, et joue sans prise de tête. Le sport collectif devient enfin simple, local et spontané.',
+  icons: {
+    icon: '/static/favicon.ico',
+  },
   title: 'Ludora – Trouve des partenaires de sport collectif près de toi',
 };
 
