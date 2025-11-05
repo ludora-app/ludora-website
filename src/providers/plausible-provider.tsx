3d60eb78b9baab34185b2278d95ef47f69622bae
hmeberbeche <hichemamir.meberbeche@cegedim-sante.com>
2025-11-05 17:07:46 +0100

    feat(plausible-setup): change matomo to plausible

--- src/providers/plausible-provider.tsx
+++ src/providers/plausible-provider.tsx
@@ -5,7 +5,7 @@ const PLAUSIBLE_SUB_DOMAIN = process.env.NEXT_PUBLIC_PLAUSIBLE_SUB_DOMAIN;
 
 export function PlausibleProvider({ children }: { children: React.ReactNode }) {
   return (
-    <PlausibleProviderNative domain={PLAUSIBLE_DOMAIN} customDomain={PLAUSIBLE_SUB_DOMAIN}>
+    <PlausibleProviderNative domain={PLAUSIBLE_DOMAIN} customDomain={PLAUSIBLE_SUB_DOMAIN} selfHosted>
       {children}
     </PlausibleProviderNative>
   );
