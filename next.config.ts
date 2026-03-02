7b64f8281d5575fa05fa5bbb5c21a3c816e8c085
amir-398 <amir.398@hotmail.fr>
2026-03-02 16:53:01 +0100

    chore: remove postHog rewrites

--- next.config.ts
+++ next.config.ts
@@ -3,19 +3,6 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'standalone',
-  async rewrites() {
-    return [
-      {
-        destination: 'https://eu-assets.i.posthog.com/static/:path*',
-        source: '/ph/static/:path*',
-      },
-      {
-        destination: 'https://eu.i.posthog.com/:path*',
-        source: '/ingest/:path*',
-      },
-    ];
-  },
-  skipTrailingSlashRedirect: true,
 };
 
 export default nextConfig;
